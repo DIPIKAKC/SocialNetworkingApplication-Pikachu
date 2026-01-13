3472afd5a5360839b283b355f9c4e7a4b81aef8e
Dipika K.C <dipikak0323@gmail.com>
2026-01-13 23:58:10 +0545

    multer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Button } from './components/ui/button'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RootLayout from "./components/RootLayout";
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        {/* test shadcn */}
-        <Button>click me button</Button>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      // {
+      //   index: true,
+      //   element: <Dashboard />
+      // }
+    ]
+  }
+
+]);
+
+export default function App() {
+  return <RouterProvider router={router} />
 }
-
-export default App
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+export default function Header() {
+    return (
+        <div className='grid grid-cols-2 h-screen'>
+            <div className='bg-amber-200'>hi</div>
+            <div className='bg-blue-100 flex items-center justify-center'>
+                <h1 className="">Pikachu</h1>
+            </div>
+        </div>
+    )
+}
