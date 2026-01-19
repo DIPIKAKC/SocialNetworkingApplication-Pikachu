3d11cf63e36eb88945f1afe538a9f93c40d5a514
Dipika K.C <dipikak0323@gmail.com>
2026-01-20 05:05:19 +0545

    update

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -4,8 +4,8 @@ import multer from "multer";
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_SECRET,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
