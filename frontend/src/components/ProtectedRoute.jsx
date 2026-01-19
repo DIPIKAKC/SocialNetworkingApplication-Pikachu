0247894c3651ee7d4d39348748ddbbbdbad9102a
Dipika K.C <dipikak0323@gmail.com>
2026-01-20 03:48:36 +0545

    protected routes

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -3,12 +3,11 @@ import { Navigate } from "react-router-dom";
 
 export const ProtectedRoute = ({ children }) => {
     const user = JSON.parse(localStorage.getItem("user"));
-
     if (!user?.token) {
         return <Navigate to="/login" replace />;
     }
-
     return children;
 };
+
 
 export default ProtectedRoute;
